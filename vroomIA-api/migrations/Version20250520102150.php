466ac534a0bb0ba635e34b9546532ae78eb3444b
AllanLoisirs <allanloisirs@MacBook-Pro-de-Allan.local>
2025-05-20 16:51:51 +0200

    gemini conversation saver

---
+++ vroomIA-api/migrations/Version20250520102150.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250520102150 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE conversation (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE message (id INT AUTO_INCREMENT NOT NULL, conversation_id INT DEFAULT NULL, owner VARCHAR(255) NOT NULL, content VARCHAR(255) NOT NULL, INDEX IDX_B6BD307F9AC0396 (conversation_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE message ADD CONSTRAINT FK_B6BD307F9AC0396 FOREIGN KEY (conversation_id) REFERENCES conversation (id)
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE message DROP FOREIGN KEY FK_B6BD307F9AC0396
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE conversation
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE message
+        SQL);
+    }
+}
--- vroomIA-api/src/Service/GeminiService.php
+++ vroomIA-api/src/Service/GeminiService.php
@@ -2,37 +2,45 @@
 
 namespace App\Service;
 
+use App\Entity\Conversation;
+use App\Entity\Message;
+use App\Entity\Role;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class GeminiService
 {
     private $client;
     private $apiKey;
+    private Conversation $conv;
 
     public function __construct(HttpClientInterface $client, string $googleGeminiApiKey)
     {
         $this->client = $client;
         $this->apiKey = $googleGeminiApiKey;
+        $this->conv = new Conversation();
     }
 
     public function generateText(string $prompt): ?string
     {
         $url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=' . $this->apiKey;
-
+        $this->conv->addMessage(new Message(Role::USER, $prompt));
         $response = $this->client->request('POST', $url, [
             'json' => [
                 'contents' => [
-                    [
-                        'parts' => [
-                            ['text' => $prompt]
-                        ]
-                    ]
+                    $this->conv->conversationToPayload()
                 ]
             ]
         ]);
 
-        $data = $response->toArray(false);
 
-        return $data['candidates'][0]['content']['parts'][0]['text'] ?? null;
+        $data = $response->toArray(false);
+        $message = $data['candidates'][0]['content']['parts'][0]['text'];
+        dump($message);
+        $this->conv->addMessage(new Message(Role::MODEL, $message));
+        return $message ?? null;
+    }
+
+    public function getConversation() {
+        return $this->conv;
     }
 }
